8db549862425d9f86d91fdcb174ed8818d614763
etherio97 <ethereal97@gmail.com>
2022-04-10 12:42:04 +0630

    z

---
+++ config.js
@@ -0,0 +1,15 @@
+exports.PORT = process.env.PORT || 5555;
+
+exports.MAIL_HOST = process.env.MAIL_HOST;
+
+exports.MAIL_PORT = process.env.MAIL_PORT;
+
+exports.MAIL_USERNAME = process.env.MAIL_USERNAME;
+
+exports.MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+
+exports.MAIL_FROM_ADDRESS = process.env.MAIL_FROM_ADDRESS;
+
+exports.MAIL_FROM_NAME = process.env.MAIL_FROM_NAME;
+
+exports.MAIL_ENCRYPTION = process.env.MAIL_ENCRYPTION;
--- server.js
+++ server.js
@@ -1,13 +1,59 @@
-const PORT = process.env.PORT || 5555;
+const {
+  PORT,
+  MAIL_HOST,
+  MAIL_PORT,
+  MAIL_USERNAME,
+  MAIL_PASSWORD,
+  MAIL_FROM_NAME,
+  MAIL_FROM_ADDRESS,
+  MAIL_ENCRYPTION,
+} = require('./config');
 
+const mailer = require('nodemailer');
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const transporter = mailer.createTransport({
+  host: MAIL_HOST,
+  port: MAIL_PORT,
+  secure: true,
+  tls: {
+    rejectUnauthorized: false,
+  },
+  auth: {
+    username: MAIL_USERNAME,
+    password: MAIL_PASSWORD,
+  },
+});
 
 app.set('trust proxy', 1);
 
-app.all('*', cors(),express.json(), express.urlencoded({ extended: true }), (req, res) => {
+app.post('/sendMail', cors(), express.json(), express.urlencoded({ extended: true }), (req, res) => {
+  let { to, cc, bbc, subject, text, html } = req.body;
+  if (!to || !subject || !(text||html)) return res.status(400).json({ error: 'Bad Request' });
+  let mail = {
+    from: MAIL_FROM_NAME + '<' + MAIL_FROM_ADDRESS + '>',
+    to,
+    cc,
+    bbc,
+    subject,
+    html,
+    text,
+  };
+  transporter.sendMail(mail, (err, info) => {
+    if (err) {
+      return res.json({
+        error: err,
+      });
+    }
+    return res.json({
+      result: info,
+    });
+  });
+});
+
+app.all('*', cors(), express.json(), express.urlencoded({ extended: true }), (req, res) => {
   let ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || req.socket.remoteAddress;
 
   res.json({
